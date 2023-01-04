200608e3affedad3c2eca254a0f81a9b60d9fcc5
gabrieldp23 <gabriel.meira.2004@gmail.com>
2023-01-04 14:56:19 -0300

    🎉 Initialize project with all presets

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Router from './routes';
+import GlobalStyle from './styles/globalStyle';
+
+function App() {
+	return (
+		<>
+			<GlobalStyle />
+			<Router />
+		</>
+	);
+}
+
+export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function Router() {
+	return (
+		<HashRouter>
+			<Routes>
+				<Route
+					path="/"
+					exact
+					element={<Home />}
+				/>
+			</Routes>
+		</HashRouter>
+	);
+}
+
+export default Router;
