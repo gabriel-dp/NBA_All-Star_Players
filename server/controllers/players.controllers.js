41af8a4a5e98e9c98a86dd5daf703d085a9d36f0
gabrieldp23 <gabriel.meira.2004@gmail.com>
2023-01-06 17:09:53 -0300

    ⚙️ Error handling in requests

--- server/controllers/players.controllers.js
+++ server/controllers/players.controllers.js
@@ -2,31 +2,46 @@ const { Types } = require('mongoose');
 const Player = require('../models/player');
 
 const getPlayer = async (req, res) => {
-	const allPlayers = await Player.find().lean().exec();
+	try {
+		const allPlayers = await Player.find().lean().exec();
 
-	res.status(200).send(allPlayers);
+		res.status(200).send(allPlayers);
+	} catch (error) {
+		res.status(500).send(`ERROR at GET getPlayer - ${error}`);
+	}
 };
 
 const createPlayer = async (req, res) => {
-	const data = req.body;
-	await Player.create(data);
+	try {
+		const data = req.body;
+		await Player.create(data);
 
-	res.status(201).send('Player created!');
+		res.status(201).send(`Player created!`);
+	} catch (error) {
+		res.status(500).send(`ERROR at POST createPlayer - ${error}`);
+	}
 };
 
 const updatePlayer = async (req, res) => {
-	const { id } = req.params;
-	const propertiers = req.body;
-	await Player.findByIdAndUpdate({ _id: Types.ObjectId(id) }, { $set: propertiers });
-
-	res.status(200).send('Player updated!');
+	try {
+		const { id } = req.params;
+		const propertiers = req.body;
+		await Player.findByIdAndUpdate({ _id: Types.ObjectId(id) }, { $set: propertiers });
+		res.status(200).send(`Player '${id}' updated!`);
+	} catch (error) {
+		res.status(500).send(`ERROR at PATCH updatePlayer - ${error}`);
+	}
 };
 
 const deletePlayer = async (req, res) => {
-	const { id } = req.params;
-	await Player.findByIdAndRemove({ _id: Types.ObjectId(id) });
+	try {
+		const { id } = req.params;
+		await Player.findByIdAndRemove({ _id: Types.ObjectId(id) });
 
-	res.status(200).send('Player deleted!');
+		res.status(200).send(`Player '${id}' deleted!`);
+	} catch (error) {
+		res.status(500).send(`ERROR at DELETE deletePlayer - ${error}`);
+	}
 };
 
 module.exports = { getPlayer, createPlayer, deletePlayer, updatePlayer };
